67dd4781733fb483b0cb2554a021e19aaed8fecc
Mohammad-Bashar <145542324+Mohammad-Bashar@users.noreply.github.com>
2026-01-29 00:30:06 +0530

    Redirect authenticated users from login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         />
        <Route
   path="/login"
-  element={<Login />}
+  element={token?<Navigate to={"/"}/>:<Login />}
 />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<PageNotFound />} />
